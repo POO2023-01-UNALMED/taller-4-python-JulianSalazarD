80fa2be000f219bebd50421d533f5351d81ffffc
Julian Salazar <jusalazard@unal.edu.co>
2023-03-03 17:44:32 -0500

    fixed up

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,12 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predenterminado", asignaturas=[], estudiantes=[]):
+    def __init__(self, grupo="grupo predeterminado", asignaturas= None, estudiantes=None):
+        if asignaturas is None:
+            asignaturas = []
+        if estudiantes is None:
+            estudiantes = []
+
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
@@ -20,15 +25,7 @@ class Grupo:
 
     def __str__(self):
         return "Grupo de estudiantes: " + self._grupo
-
-    #@ classmethod
-    #def asignarNombre(cls, nombre="Grado 10"):
-    #    cls.grado = nombre
-
+    
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
-
-    #@ classmethod
-    #def asignarNombre(cls, nombre="Grado 4"):
-    #    cls.grado = nombre
